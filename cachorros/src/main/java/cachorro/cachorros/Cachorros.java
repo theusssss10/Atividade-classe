590371b9688573494f0119b7bdf0367ed04699d7
theusssss10 <108599997+theusssss10@users.noreply.github.com>
2023-03-08 20:24:18 -0300

    Add files via upload
    Atividade Classes

---
+++ cachorros/src/main/java/cachorro/cachorros/Cachorros.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package cachorro.cachorros;
+
+/**
+ *
+ * @author aluno
+ */
+public class Cachorros {
+    
+    int idade;
+    String nome;
+    double peso;
+    double altura;
+    int qtdPatas;
+    
+    }
+
---
+++ cachorros/src/main/java/cachorro/cachorros/testeCachorro.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cachorro.cachorros;
+
+/**
+ *
+ * @author aluno
+ */
+public class testeCachorro {
+    public static void main(String[]args){
+        
+        Cachorros tipo=new Cachorros();
+        tipo.idade=12;
+        tipo.nome="Bob";
+        tipo.peso=25;
+        tipo.altura=45.0;
+        tipo.qtdPatas=4;
+        
+        Cachorros tipo1=new Cachorros();
+        tipo1.idade=15;
+        tipo1.nome="Minie";
+        tipo1.peso=10;
+        tipo1.altura=24.0;
+        tipo1.qtdPatas=4;
+        
+        Cachorros tipo2=new Cachorros();
+        tipo2.idade=2;
+        tipo2.nome="Caca";
+        tipo2.peso=5;
+        tipo2.altura=12.0;
+        tipo2.qtdPatas=4;
+        
+        Cachorros tipo3=new Cachorros();
+        tipo3.idade=4;
+        tipo3.nome="Pol";
+        tipo3.peso=8;
+        tipo3.altura=33.0;
+        tipo3.qtdPatas=4;
+        
+        Cachorros tipo4=new Cachorros();
+        tipo4.idade=9;
+        tipo4.nome="Cao";
+        tipo4.peso=40;
+        tipo4.altura=70.0;
+        tipo4.qtdPatas=3;
+        
+    }
+}
